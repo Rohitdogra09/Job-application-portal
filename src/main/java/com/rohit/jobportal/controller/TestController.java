157caac189a3979b24f2a0c1aa44c407424e8fac
Rohit Dogra <dograr087@gmail.com>
2026-01-29 19:21:46 +0100

    initial commit

--- src/main/java/com/rohit/jobportal/controller/TestController.java
+++ src/main/java/com/rohit/jobportal/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.rohit.jobportal.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
