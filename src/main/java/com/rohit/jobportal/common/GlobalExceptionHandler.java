d95cb6ca1d674683705582e4bb84f4e2daa0e167
Rohit Dogra <dograr087@gmail.com>
2026-01-31 22:17:22 +0100

    initial commit

---
+++ src/main/java/com/rohit/jobportal/common/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.rohit.jobportal.common;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, Object> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fe : ex.getBindingResult().getFieldErrors()) {
+            errors.put(fe.getField(), fe.getDefaultMessage());
+        }
+        return Map.of(
+                "message", "Validation failed",
+                "errors", errors
+        );
+    }
+}
